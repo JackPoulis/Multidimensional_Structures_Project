8c4a6b26fa60b6864407a726368767c03ad403a3
Iakovos Poulis <jackpoulis@gmail.com>
2022-02-03 21:04:13 +0200

    Close files after vectorization

--- main.py
+++ main.py
@@ -7,3 +7,4 @@ import tools
 fileNames = tools.getListOfFiles(".\\sample_documents")
 inputList = [open(filename, 'r', encoding='utf-8', errors='ignore') for filename in fileNames]
 datapoints = tools.vectorize(inputList, input='file')
+[f.close() for f in inputList]
