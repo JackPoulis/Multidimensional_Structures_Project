c09666efa062541fe4fb78ec4b8cc007c8d2d10f
Iakovos Poulis <jackpoulis@gmail.com>
2022-09-26 17:57:12 +0300

    Improve main.py parameterization

--- main.py
+++ main.py
@@ -14,16 +14,18 @@ With this script we can measure the size of the tree generated and
 compare it with the space complexity formulas that each tree has
 """
 
-max_N = 150 #max number of documents
-min_N = 10 #min number of documents
-test_step = 5
-dimensions = 10
+max_N = 45 #max number of datapoints
+min_N = 10 #min number of datapoints
+test_step = 1
+dimensions = 2
 test_range = range(min_N, max_N+1, test_step)
+test_tree = "Range tree" #Options are 'KD tree', 'Quad tree', 'Range tree'
+#If tree is quad tree dimensions should not be more than 2
 
-fileNames = list_files(".\\samples\\samples1")
+fileNames = list_files(".\\samples\\samples1")# The documents directory from which to generate the points
 inputList = [open(filename, 'r', encoding='utf-8', errors='ignore') for filename in fileNames]
 datapoints, features = vectorize(inputList, input='file', max_features=dimensions)
-datapoints = compress(datapoints)
+datapoints = compress(datapoints) #Group duplicates 
 [f.close() for f in inputList]
 
 d = len(features) #Dimensions
@@ -34,30 +36,41 @@ quadtree_sizes = []
 rangetree_sizes = []
 
 for N in test_range:
-    #Quad tree boundary (In case we measure quad trees)
-    # mbr = calc_mbr(datapoints[:N])
-    # boundary = Rect(math.ceil(mbr[0][1]/2), math.ceil(mbr[1][1]/2), mbr[0][1]-mbr[0][0]+1, mbr[1][1]-mbr[1][0]+1)
 
-    kd_tree = kdtree.KDTree(datapoints[:N])
-    # quad_tree = quadtree.QuadTree(boundary, datapoints[:N])
-    # range_tree = rangetree.RangeTree(datapoints[:N])
+    if test_tree=='KD tree':
+        kd_tree = kdtree.KDTree(datapoints[:N])
+        kdtree_sizes.append(kd_tree.size())
+    elif test_tree=='Quad tree':
+        mbr = calc_mbr(datapoints[:N])
+        boundary = Rect(math.ceil(mbr[0][1]/2), math.ceil(mbr[1][1]/2), mbr[0][1]-mbr[0][0]+1, mbr[1][1]-mbr[1][0]+1)
+        quad_tree = quadtree.QuadTree(boundary, datapoints[:N])
+        quadtree_sizes.append(quad_tree.size())
+    elif test_tree=='Range tree':
+        range_tree = rangetree.RangeTree(datapoints[:N])
+        rangetree_sizes.append(range_tree.size()) 
 
-    kdtree_sizes.append(kd_tree.size())
-    # quadtree_sizes.append(quad_tree.size())
-    # rangetree_sizes.append(range_tree.size()) 
-
-#space complexity labels
-kdtree_label = r'$4{n}$'
-quadtree_label = r'$\frac{n}{2}$'
-rangetree_label = r'$3{n}(\frac{log({n})}{log(log({n}))})^{{d}-1}$'
+if test_tree=='KD tree':
+    complexity = kdtree_space_complexity
+    formula = r'${n}$'
+    a_points = kdtree_sizes
+elif test_tree=='Quad tree':
+    complexity = quadtree_space_complexity
+    formula = r'${n}$'
+    a_points = quadtree_sizes
+elif test_tree=='Range tree':
+    complexity = rangetree_space_complexity
+    formula = r'${n}(\frac{log({n})}{log(log({n}))})^{{d}-1}$'
+    a_points = rangetree_sizes
+else:
+    complexity = lambda x : x
+    formula = r'${n}$'
 
 linspace = np.linspace(4,max_N,100)
 x_axis = test_range
-a_points = kdtree_sizes
-b_line = [kdtree_space_complexity(x) for x in linspace]
+b_line = [complexity(x) for x in linspace]
 plt.scatter(x_axis, a_points, s=120, alpha=1)
-plt.plot(linspace, b_line, color="red", label=quadtree_label, linewidth=3)
-plt.title("KD tree space complexity", size=18)
+plt.plot(linspace, b_line, color="red", label=formula, linewidth=3)
+plt.title(test_tree + " space complexity", size=18)
 plt.xlabel('n', size=18)
 plt.ylabel('number of nodes in tree', size=18)
 plt.legend(prop={"size":16}, loc='upper left')
